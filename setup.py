fca6ee4fc5dc2d75a0356d9fb23279984c2f19db
Ilya Vihtinsky <iliaviht@gmail.com>
2018-01-12 16:28:36 +0300

    Changed version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mongosql',
-    version='1.3.3-0',
+    version='1.3.4-0',
     author='Mark Vartanyan',
     author_email='kolypto@gmail.com',
 
