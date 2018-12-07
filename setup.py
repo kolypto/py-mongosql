76e802e29b7d94ccc28cd84dea129c0c34a98b13
Ilya Vihtinsky <iliaviht@gmail.com>
2018-12-07 21:30:33 +0300

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mongosql',
-    version='1.4.3-0',
+    version='1.4.4-0',
     author='Mark Vartanyan',
     author_email='kolypto@gmail.com',
 
