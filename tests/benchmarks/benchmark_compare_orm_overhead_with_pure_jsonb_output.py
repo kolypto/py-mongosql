cd8f8b38abb0f1f2fbc10038453355c44478c10b
Mark Vartanyan <kolypto@gmail.com>
2020-09-15 00:04:29 +0300

    More benchmarks. LEFT JOIN + manual parsing is still the fastest way

--- tests/benchmarks/benchmark_compare_orm_overhead_with_pure_jsonb_output.py
+++ tests/benchmarks/benchmark_compare_orm_overhead_with_pure_jsonb_output.py
@@ -7,13 +7,20 @@ We use several different ways to do it, and compare the results.
 
 The results are quite interesting:
 
-test_load_related__joinedload: 32.22s (447.81%)  -- use SqlAlchemy + joinedload()
-test_load_related__selectinload: 38.98s (541.70%) -- use SqlAlchemy + selectinload()
-test_load_related__no_sqlalchemy__left_join: 7.20s (100.00%) -- use plain SQL, no ORM
-test_load_related__JSONB_tuples: 25.41s (353.18%) -- form JSONB in postgres
-test_load_related__JSONB_objects: 39.91s (554.60%) -- form JSONB in postgres, also use to_jsonb() on rows to get the keys
-test_load_related__JSON_tuples: 11.72s (162.93%) -- form JSON in postgres
-test_load_related__JSON_objects: 19.18s (266.60%) -- form JSON in postgres, also use to_json() on rows to get the keys
+test_joinedload: 15.40s (395.12%)
+test_selectinload: 18.73s (480.50%)
+test_core__left_join_with_python_nesting: 3.90s (100.00%)
+test_core__3_queries__tuples: 4.57s (117.38%)
+test_core__3_queries__json: 10.98s (281.63%)
+test_subquery_jsonb_tuples: 12.35s (316.98%)
+test_subquery_jsonb_objects: 20.82s (534.27%)
+test_subquery_json_tuples: 6.05s (155.31%)
+test_subquery_json_objects: 9.44s (242.32%)
+test_single_line_agg__json: 7.31s (187.58%)
+test_single_line_agg__jsonb: 14.42s (369.89%)
+test_semisingle_line_agg__json: 7.34s (188.41%)
+test_semisingle_line_agg__jsonb: 15.40s (395.09%)
+
 
 What that means is:
 
@@ -45,10 +52,11 @@ to those queries that feed the data to JSON immediately. It's worth doing, but i
 The problems with this approach: we'll have to make sure that `@property` fields are included into the results if
 they were specified in a projection.
 """
+
 from sqlalchemy.orm import selectinload, joinedload
 
 from tests.benchmarks.benchmark_utils import benchmark_parallel_funcs
-from tests.models import get_big_db_for_benchmarks, User, Article, Comment
+from tests.models import get_big_db_for_benchmarks, User, Article
 
 # Init DB
 engine, Session = get_big_db_for_benchmarks(n_users=100,
@@ -56,41 +64,110 @@ engine, Session = get_big_db_for_benchmarks(n_users=100,
                                             n_comments_per_article=3)
 
 # Prepare
-N_REPEATS = 1000
+N_REPEATS = 500
 ssn = Session()
 
 
 # Tests
-def test_load_related__selectinload(n):
+def test_selectinload(n):
     """ Load Users+Articles+Comments, with selectinload() """
     for i in range(n):
-        users = list(ssn.query(User).options(
+        users = ssn.query(User).options(
             selectinload(User.articles).selectinload(Article.comments)
-        ))
+        ).all()
 
-def test_load_related__no_sqlalchemy__left_join(n):
-    """ Load Users+Articles+Comments: a plain SQL query to estimate SqlAlchemy's overhead. LEFT JOIN. """
+
+def test_joinedload(n):
+    """ Load Users+Articles+Comments, with joinedload() """
     for i in range(n):
-        list(ssn.execute("""
+        users = ssn.query(User).options(
+            joinedload(User.articles).joinedload(Article.comments)
+        ).all()
+
+
+def test_core__left_join_with_python_nesting(n):
+    """ Use a plain SQL query with LEFT JOIN + generate JSON objects in Python """
+    for i in range(n):
+        rows = ssn.execute("""
         SELECT u.*, a.*, c.* 
         FROM u
         LEFT JOIN a ON u.id = a.uid
         LEFT JOIN c ON c.aid = a.id
-        """))
+        """).fetchall()
 
-def test_load_related__joinedload(n):
-    """ Load Users+Articles+Comments, with joinedload() """
+        # Okay, that was fast; what if we collect all the data in Python?
+        # Will Python loops kill all performance?
+
+        users = []
+        user_id_to_user = {}
+        article_id_to_article = {}
+        comment_id_to_comment = {}
+
+        for row in rows:
+            # user dict()
+            user_id = row[0]
+            if user_id in user_id_to_user:
+                user = user_id_to_user[user_id]
+            else:
+                user = {'id': row[0], 'name': row[1], 'tags': row[2], 'age': row[3], 'articles': []}
+                user_id_to_user[user_id] = user
+                users.append(user)
+
+            # article dict()
+            article_id = row[4]
+            if article_id:
+                if article_id in article_id_to_article:
+                    article = article_id_to_article[article_id]
+                else:
+                    article = {'id': row[4], 'uid': row[5], 'title': row[6], 'theme': row[7], 'data': row[8], 'comments': []}
+                    article_id_to_article[article_id] = article
+                if user:
+                    user['articles'].append(article)
+            else:
+                article = None
+
+            # comment dict()
+            comment_id = row[9]
+            if comment_id:
+                if comment_id in comment_id_to_comment:
+                    comment = comment_id_to_comment[comment_id]
+                else:
+                    comment ={'id': row[9], 'aid': row[10], 'uid': row[11], 'text': row[12]}
+                if article:
+                    article['comments'].append(comment)
+            else:
+                comment = None
+
+
+def test_core__3_queries__tuples(n):
+    """ Make 3 queries, load tuples """
     for i in range(n):
-        users = list(ssn.query(User).options(
-            joinedload(User.articles).joinedload(Article.comments)
-        ))
+        users = ssn.execute('SELECT u.* FROM u;').fetchall()
+
+        user_ids = set(str(u.id) for u in users)
+        articles = ssn.execute('SELECT a.* FROM a WHERE uid IN (' + (','.join(user_ids)) + ')').fetchall()
+
+        article_ids = set(str(a.id) for a in articles)
+        comments = ssn.execute('SELECT c.* FROM c WHERE aid IN (' + (','.join(article_ids)) + ')').fetchall()
+
+
+def test_core__3_queries__json(n):
+    """ Make 3 queries, load json rows """
+    for i in range(n):
+        users = ssn.execute('SELECT to_json(u) FROM u;').fetchall()
+
+        user_ids = set(str(u[0]['id']) for u in users)
+        articles = ssn.execute('SELECT to_json(a) FROM a WHERE uid IN (' + (','.join(user_ids)) + ')').fetchall()
+
+        article_ids = set(str(a[0]['id']) for a in articles)
+        comments = ssn.execute('SELECT to_json(c) FROM c WHERE aid IN (' + (','.join(article_ids)) + ')').fetchall()
 
 # Now do the same with with nested JSON
 # We query Users, their Articles, and their Comments, all as nested objects
 # We use `AGG_FUNCTION`, which is either json_agg(), or jsonb_agg()
 # We use different `*_SELECT` expressions, which are either a list of columns, or to_json(row), or to_jsonb(row)
 # All those combinations we test to see which one is faster
-QUERY_TEMPLATE = """
+NESTED_AGG_QUERY_TEMPLATE = """
 SELECT
     {USERS_SELECT},
     {AGG_FUNCTION}(articles_q) AS articles
@@ -110,9 +187,9 @@ LEFT JOIN (
 GROUP BY users.id;
 """
 
-def test_load_related__JSONB_tuples(n):
+def test_subquery_jsonb_tuples(n):
     """ Test making JSONB on the server. Return tuples. """
-    query = QUERY_TEMPLATE.format(
+    query = NESTED_AGG_QUERY_TEMPLATE.format(
         # Use JSONB for nested objects
         AGG_FUNCTION='jsonb_agg',
         # Select rows as tuples
@@ -120,14 +197,13 @@ def test_load_related__JSONB_tuples(n):
         ARTICLES_SELECT='articles.*',
         COMMENTS_SELECT='comments.*',
     )
-
     for i in range(n):
-        users = list(ssn.execute(query))
+        users = ssn.execute(query).fetchall()
 
 
-def test_load_related__JSONB_objects(n):
+def test_subquery_jsonb_objects(n):
     """ Test making JSONB on the server. Make objects with row_to_json() """
-    query = QUERY_TEMPLATE.format(
+    query = NESTED_AGG_QUERY_TEMPLATE.format(
         # Use JSONB for nested objects
         AGG_FUNCTION='jsonb_agg',
         # Select rows as JSONB objects
@@ -140,9 +216,9 @@ def test_load_related__JSONB_objects(n):
     for i in range(n):
         users = list(ssn.execute(query))
 
-def test_load_related__JSON_tuples(n):
+def test_subquery_json_tuples(n):
     """ Test making JSON on the server. Return tuples. """
-    query = QUERY_TEMPLATE.format(
+    query = NESTED_AGG_QUERY_TEMPLATE.format(
         # Use JSON for nested objects
         AGG_FUNCTION='json_agg',
         # Select rows as tuples
@@ -150,13 +226,12 @@ def test_load_related__JSON_tuples(n):
         ARTICLES_SELECT='articles.*',
         COMMENTS_SELECT='comments.*',
     )
-
     for i in range(n):
         users = list(ssn.execute(query))
 
-def test_load_related__JSON_objects(n):
+def test_subquery_json_objects(n):
     """ Test making JSONB on the server. Make objects with row_to_json() """
-    query = QUERY_TEMPLATE.format(
+    query = NESTED_AGG_QUERY_TEMPLATE.format(
         # Use JSON for nested objects
         AGG_FUNCTION='json_agg',
         USERS_SELECT='users.id, to_json(users) AS user',
@@ -170,16 +245,82 @@ def test_load_related__JSON_objects(n):
         users = list(ssn.execute(query))
 
 
+LINE_AGG_QUERY_TEMPLATE = """
+SELECT {TO_JSON}(u), {JSON_AGG}(a), {JSON_AGG}(c) 
+FROM u 
+    LEFT JOIN a ON(u.id=a.uid) 
+    LEFT JOIN c ON (a.id=c.aid) 
+GROUP BY u.id;
+"""
+
+def test_single_line_agg__json(n):
+    """ Linear aggregation: no nesting; everything's aggregated into separate JSON lists """
+    query = LINE_AGG_QUERY_TEMPLATE.format(
+        TO_JSON='to_json',
+        JSON_AGG='json_agg',
+    )
+    for i in range(n):
+        rows = ssn.execute(query).fetchall()
+
+def test_single_line_agg__jsonb(n):
+    """ Linear aggregation: no nesting; everything's aggregated into separate JSONB lists """
+    query = LINE_AGG_QUERY_TEMPLATE.format(
+        TO_JSON='to_jsonb',
+        JSON_AGG='jsonb_agg',
+    )
+    for i in range(n):
+        rows = ssn.execute(query).fetchall()
+
+
+SEMILINE_AGG_QUERY_TEMPLATE = """
+SELECT 
+    {JSON_BUILD_OBJECT}(
+        'id', u.id, 'name', u.name, 'tags', u.tags, 'age', u.age, 
+        'articles', {JSON_AGG}(a)), 
+    {JSON_AGG}(c) 
+FROM u 
+    LEFT JOIN a ON(u.id=a.uid) 
+    LEFT JOIN c ON (a.id=c.aid) 
+GROUP BY u.id;
+"""
+
+def test_semisingle_line_agg__json(n):
+    """ Aggregate only 1st level objects; things that are nested deeper are expelled to the outskirts """
+    query = SEMILINE_AGG_QUERY_TEMPLATE.format(
+        JSON_BUILD_OBJECT='json_build_object',
+        JSON_AGG='json_agg',
+    )
+    for i in range(n):
+        rows = ssn.execute(query).fetchall()
+
+
+def test_semisingle_line_agg__jsonb(n):
+    """ Aggregate only 1st level objects; things that are nested deeper are expelled to the outskirts """
+    query = SEMILINE_AGG_QUERY_TEMPLATE.format(
+        JSON_BUILD_OBJECT='jsonb_build_object',
+        JSON_AGG='jsonb_agg',
+    )
+    for i in range(n):
+        rows = ssn.execute(query).fetchall()
+
+
+
 # Run
 res = benchmark_parallel_funcs(
     N_REPEATS, 10,
-    test_load_related__joinedload,
-    test_load_related__selectinload,
-    test_load_related__no_sqlalchemy__left_join,
-    test_load_related__JSONB_tuples,
-    test_load_related__JSONB_objects,
-    test_load_related__JSON_tuples,
-    test_load_related__JSON_objects,
+    test_joinedload,
+    test_selectinload,
+    test_core__left_join_with_python_nesting,
+    test_core__3_queries__tuples,
+    test_core__3_queries__json,
+    test_subquery_jsonb_tuples,
+    test_subquery_jsonb_objects,
+    test_subquery_json_tuples,
+    test_subquery_json_objects,
+    test_single_line_agg__json,
+    test_single_line_agg__jsonb,
+    test_semisingle_line_agg__json,
+    test_semisingle_line_agg__jsonb,
 )
 
 # Done
