677abc0968afadb2156265474731c5690b1c4843
Mark Vartanyan <kolypto@gmail.com>
2020-09-26 08:40:51 +0300

    Project template

---
+++ README.md
@@ -0,0 +1,2 @@
+[![Tests](https://github.com/kolypto/py-project/workflows/Tests/badge.svg)](/kolypto/py-project/actions)
+[![Pythons](https://img.shields.io/badge/python-3.7%E2%80%933.8-blue.svg)](noxfile.py)
---
+++ noxfile.py
@@ -0,0 +1,15 @@
+import nox.sessions
+
+
+nox.options.reuse_existing_virtualenvs = True
+nox.options.sessions = ['tests']
+
+
+@nox.session(python=['3.7', '3.8'])
+def tests(session: nox.sessions.Session):
+    """ Run all tests """
+    session.install('poetry')
+    session.run('poetry', 'install')
+
+    # Test
+    session.run('pytest', 'tests/', '--cov=project')
