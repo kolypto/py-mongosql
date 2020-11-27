5f651c24c2f36717ec295feb224fd23b8b3bd94e
Mark <kolypto@gmail.com>
2020-11-27 16:25:32 +0300

    noxfile: better syntax

--- noxfile.py
+++ noxfile.py
@@ -1,15 +1,30 @@
 import nox.sessions
 
+PYTHON_VERSIONS = ['3.7', '3.8', '3.9']
+SQLALCHEMY_VERIONS = [
+    *(f'1.3.{x}' `for x in range(0, 1 + 20)),
+]
 
 nox.options.reuse_existing_virtualenvs = True
 nox.options.sessions = ['tests']
 
 
-@nox.session(python=['3.7', '3.8'])
-def tests(session: nox.sessions.Session):
+@nox.session(python=PYTHON_VERSIONS)
+def tests(session: nox.sessions.Session, sqlalchemy=None):
     """ Run all tests """
     session.install('poetry')
     session.run('poetry', 'install')
+    
+    # Specific package versions
+    if sqlalchemy:
+        session.install(f'sqlalchemy=={sqlalchemy}')
 
     # Test
     session.run('pytest', 'tests/', '--cov=myproject')
+
+
+@nox.session(python=PYTHON_VERSIONS[-1])
+@nox.parametrize('sqlalchemy', SQLALCHEMY_VERIONS)
+def tests_sqlalchemy(session: nox.sessions.Session, sqlalchemy):
+    """ Test against a specific SqlAlchemy version """
+    tests(session, sqlalchemy)
